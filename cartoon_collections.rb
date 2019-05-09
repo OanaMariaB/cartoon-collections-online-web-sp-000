739f9a05579abf1d438124c55dc5c83114fda8ac
Oana Berentan <oana.berentan@yahoo.com>
2019-05-09 19:35:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,10 +13,8 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
-  if calls.length > 4
-    return true
-  else
-    return false
+  calls.any? do |call|
+    call.length > 4
   end
 end
 
