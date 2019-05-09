9ea891d53b240652353375f44ea3cd303c1ba3cf
Oana Berentan <oana.berentan@yahoo.com>
2019-05-09 19:32:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,12 @@ def summon_captain_planet(planeteer_calls)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  if calls.length < 4
+    return false
+  else
+    return true
+  end
 end
 
 def find_the_cheese# code an argument here
